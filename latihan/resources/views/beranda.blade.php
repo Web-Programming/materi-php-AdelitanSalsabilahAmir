190187316ebfef80eb6702b926c69538114507a9
TwinDragonFangs <161033764+TwinDragonFangs@users.noreply.github.com>
2025-04-29 20:49:13 +0700

    commit belajar php

---
+++ latihan/resources/views/beranda.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <h1>welcome to adelitan home</h1>
+    <h2>hello {{ $name }}</h2>
+    <h2>your email is {{ $email }}</h2>
+    <h2>your addres is {{ $alamat }}</h2>
+</div>
--- latihan/routes/web.php
+++ latihan/routes/web.php
@@ -5,3 +5,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('beranda', function () {
+    return view('beranda',
+    [
+        'name' => 'adelitan',
+        'email' => 'adelitanbintiamir@gmail.com',
+        'alamat' => 'bukit'
+    ]
+);
+});
+Route::get('/berita/{id}/{judul}',function ($id, $judul){
+    return view('berita',['id' => $id, 'judul' => $judul]);
+});    
+
+
+
