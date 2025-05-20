3f5b01b8d73d99a6d5ebb01e54add5960de84d48
TwinDragonFangs <adelitanbintiamir@gmail.com>
2025-05-20 20:32:39 +0700

    commit crud

--- latihan/routes/web.php
+++ latihan/routes/web.php
@@ -1,37 +1,27 @@
 <?php
 
 use App\Http\Controllers\ProdiController;
-use App\Http\Controllers\MateriController;
-use App\Http\Controllers\FakultasController;
-use App\Http\Controllers\MahasiswaController;
-use App\Http\Controllers\DosenController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('beranda', function () {
-    return view('beranda',
+
+Route::get('/home', function(){
+    return view('beranda', 
     [
-        'name' => 'adelitan',
-        'email' => 'adelitanbintiamir@gmail.com',
-        'alamat' => 'bukit'
-    ]
-);
+            'name' => 'Adelitan',
+            'email' => 'adelitanbintiamir@gmail.com',
+            'alamat' => 'Palembang'
+        ]
+    );
 });
-Route::get('/berita/{id}/{judul}',function ($id, $judul){
-    return view('berita',['id' => $id, 'judul' => $judul]);
-});    
 
+Route::get('/berita/{id}/{judul?}', function($id, $judul = null){
+    return view('berita', ['id' => $id, 'judul' => $judul]);
+});
 
-//membuat route ke halaman prodi index melalui controller ProdiController
-Route::get('/prodi/index', action: [ProdiController::class,'index']);
+//membuat route ke halam prodi index melalui controller ProdiController
+//Route::get('/prodi/index', [ProdiController::class,'index']);
 
-Route::resource('materi', MateriController::class);
 Route::resource('prodi', ProdiController::class);
-Route::resource('fakultas', FakultasController::class);
-Route::resource('mhs', MahasiswaController::class); // ganti dari 'mahasiswa' ke 'mhs'
-Route::resource('dosen', DosenController::class);
-
-
-
